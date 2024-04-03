4e49d79ce9a9d62f8f9e31b883c2e34ec0a1f1f2
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-04-03 12:51:09 +0900

    윈도우 닫기 버튼 구현

--- Scripts/window.js
+++ Scripts/window.js
@@ -1,13 +1,15 @@
 const controlTodo = document.querySelectorAll("#todo .controls .contents .material-icons");
+const exitButton = document.querySelectorAll("#todo .window .close");
 const todo = document.querySelector("#todo .window.todo");
 const timeTable = document.querySelector("#todo .window.timeTable");
 
 const TODO_BUTTON = 2;
 const TIMETABLE_BUTTON = 3;
+const TODO_EXIT = 0;
+const TIMETABLE_EXIT = 1;
 
 function handleTodoWindow() {
   todo.classList.toggle("hidden");
-  console.log("click");
 }
 
 function handleTimeTableWindow() {
@@ -16,3 +18,5 @@ function handleTimeTableWindow() {
 
 controlTodo[TODO_BUTTON].addEventListener("click", handleTodoWindow);
 controlTodo[TIMETABLE_BUTTON].addEventListener("click", handleTimeTableWindow);
+exitButton[TODO_EXIT].addEventListener("click", handleTodoWindow);
+exitButton[TIMETABLE_EXIT].addEventListener("click", handleTimeTableWindow);
